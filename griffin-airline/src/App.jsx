622120bfc7fd9b9fdbd7c37eeecd902c2ea6dc50
Ayeh <119679662+Ayehmozaffari@users.noreply.github.com>
2024-09-06 13:21:26 +0430

    add home into app.jsx

--- griffin-airline/src/App.jsx
+++ griffin-airline/src/App.jsx
@@ -1,9 +1,11 @@
-import React  from 'react'
+import React  from 'react';
 import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
 const App=()=>{
   return(
     <div>
       <Navbar/>
+      <Home/>
     </div>
     
   )
