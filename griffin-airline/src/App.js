386c4aa4334adb33c840ee6403a1773fc79c79b6
Ayeh <119679662+Ayehmozaffari@users.noreply.github.com>
2024-09-19 20:24:35 +0430

    hello guys

--- griffin-airline/src/App.js
+++ griffin-airline/src/App.js
@@ -1,4 +1,19 @@
 import React from 'react';
+import Hero from './components/Hero';
+import ScrollToTop from './components/ScrollToTop';
+import Services from './components/Services';
+import Navbar from './components/Navbar';
+import Recommend from './components/Recommend';
+import Testimonials from './components/Testimonials';
+import Footer from './components/Footer';
 export default function App (){
-  return <div>سلام</div>
+  return <>
+  <ScrollToTop />
+  <Navbar />
+  <Hero />
+  <Services />
+  <Recommend />
+  <Testimonials />
+  <Footer />
+  </>
 }
--- griffin-airline/src/components/Navbar.jsx
+++ griffin-airline/src/components/Navbar.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
-
+import styled from'styled-components';
+import logo from '../assets/site-logo.png';
 export default function Navbar() {
   return (
-    <div>
-      
-    </div>
+   <>
+    <Nav>
+      <div className="brand">
+        <div className="container">
+          <img src="{logo}" alt="" />
+          شــیردال
+        </div>
+        <div className="toggle"></div>
+      </div>
+      <ul>
+        <li>
+          <a href="#home">خـانه</a>
+          </li>
+        <li>
+          <a href="#services">سرویـس هـا</a>
+        </li>
+        <li>
+          <a href="#recommend"> مکان هـا</a>
+        </li>
+        <li>
+          <a href="#testimonials">سفارش</a>
+        </li>
+      </ul>
+      <button>ارتباط</button>
+      </Nav>
+      </>
   )
 }
+const Nav =styled.nav``;
