c22e331cb070bd3da6a4806e853ce3a6a81cbb4f
Ayeh <119679662+Ayehmozaffari@users.noreply.github.com>
2024-09-21 15:26:12 +0330

    add some changes
    because we didn't have time to send other commits
    now I'll send all of them

--- app.js
+++ app.js
@@ -6,3 +6,17 @@ menuBtn.addEventListener("click", (e)=>{
     const isOpen= navLinks.classList.contains("open");
     menuBtnIcon.setAttribute("class", isOpen ?'icons8-close':'icons8-menu')
 });
+navLinks.addEventListener("click" ,(e)=>{
+    navLinks.classList.remove("open");
+    menuBtnIcon.setAttribute("class","icons8-close")
+});
+// const scrollRevealOption ={
+//     origin:"bottom",
+//     distance:"50px",
+//     duration:1000,
+
+// }
+// scrollReveal().reveal(".header__image img" ,{
+//     ...scrollRevealOption,
+//     origin:"right",
+// });
